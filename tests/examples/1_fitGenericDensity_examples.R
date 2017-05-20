953fae575e4ddfb923848bd0a82c9fb6d5a147ff
chizhangucb <chi.zhang@berkeley.edu>
2017-05-19 18:45:24 -0700

    Create 1_fitGenericDensity_examples.R

---
+++ tests/examples/1_fitGenericDensity_examples.R
@@ -0,0 +1,29 @@
+### Load data
+# A is normal with mu for each observation being a function of (W1, W2, W3, W4), sd = 1;
+data(sampleDat_iidcontABinY)
+dat_iidcontABinY <- sampleDat_iidcontABinY$dat_iidcontABinY
+psi0.Y <- sampleDat_iidcontABinY$psi0.Y  # 0.291398
+psi0.Ygstar <- sampleDat_iidcontABinY$psi0.Ygstar  # 0.316274
+
+### Define a stochastic intervention
+define_f.gstar <- function(shift.val, truncBD, rndseed = NULL) {
+  shift.const <- shift.val
+  trunc.const <- truncBD
+  f.gstar <- function(data, ...) {
+    print("shift.const: " %+% shift.const)
+    set.seed(rndseed)
+    A.mu <- 0.86 * data[,"W1"] + 0.41 * data[,"W2"] - 0.34 * data[,"W3"] + 0.93 * data[,"W4"]
+    untrunc.A <- rnorm(n = nrow(data), mean = A.mu + shift.const, sd = 1)
+    r.new.A <- exp(0.8 * shift.const * (untrunc.A - A.mu - shift.const / 3))
+    trunc.A <- ifelse(r.new.A > trunc.const, untrunc.A - shift.const, untrunc.A)
+    return(trunc.A)
+  }
+  return(f.gstar)
+}
+f.gstar <- define_f.gstar(shift = sampleDat_iidcontABinY$shift.val, truncBD = sampleDat_iidcontABinY$truncBD, 
+                          rndseed = sampleDat_iidcontABinY$rndseed)
+
+### Run
+tmleCom_Options(maxNperBin = nrow(dat_iidcontABinY))
+h_gN <- fitGenericDensity(data, Anodes = "A", Wnodes = c("W1", "W2", "W3", "W4"), f_gstar = NULL, lbound = 0)$h_gstar
+h_gstar <- fitGenericDensity(data, Anodes = "A", Wnodes = c("W1", "W2", "W3", "W4"), f_gstar = f.gstar, lbound = 0)$h_gstar
