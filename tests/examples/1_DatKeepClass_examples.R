9750e179c73505efccd098c5851fda080ed84a89
chizhangucb <chi.zhang@berkeley.edu>
2017-07-31 23:52:23 -0700

    Update 1_DatKeepClass_examples.R

--- tests/examples/1_DatKeepClass_examples.R
+++ tests/examples/1_DatKeepClass_examples.R
@@ -20,15 +20,18 @@ define_f.gstar <- function(shift.val, truncBD, rndseed = NULL) {
   }
   return(f.gstar)
 }
-f.gstar <- define_f.gstar(shift = sampleDat_iidcontAContY$shift.val, truncBD = sampleDat_iidcontAContY$truncBD, 
+f.gstar <- define_f.gstar(shift = sampleDat_iidcontAContY$shift.val, 
+                          truncBD = sampleDat_iidcontAContY$truncBD, 
                           rndseed = sampleDat_iidcontAContY$rndseed)
 
-tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm", maxNperBin = nrow(dat_iidcontAContY))
-OData <- DatKeepClass$new(Odata = dat_iidcontAContY, nodes = nodes, norm.c.sVars = FALSE)  # don't normalize continous covariates here
-nobs <- OData$nobs; nobs # 10000
+tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm", 
+                maxNperBin = nrow(dat_iidcontAContY))
+# Don't normalize continous covariates here
+OData <- DatKeepClass$new(Odata = dat_iidcontAContY, nodes = nodes, norm.c.sVars = FALSE)  
 OData$names.sVar  # "Y"  "A"  "W1" "W2" "W3" "W4" (names of all variables in input data )
 OData$names.c.sVar  # "A"  "W3" "W4" (names of all continuous variables in input data)
 head(OData$dat.sVar)  # a subset data.frame of Odata that includes all variables in nodes
+nobs <- OData$nobs; nobs # 10000
 
 obsYvals <- dat_iidcontAContY[, nodes$Ynode]
 OData$addYnode(YnodeVals = obsYvals)  # The same as OData$addYnode(YnodeVals = obsYvals, det.Y = F)
@@ -42,7 +45,8 @@ OData$get.sVar.type()  # Provide a list of types of all variables
 
 OData.gstar <- DatKeepClass$new(Odata = dat_iidcontAContY, nodes = nodes, norm.c.sVars = FALSE)
 # Create (p=) 1 new Odata (nobs obs at a time) and replace A under g0 in Odata with A^* under g.star
-OData.gstar$make.dat.sVar(p = 1, f.g_fun = f.gstar)  # Generate new exposures under user-specific arbitrary intervention f.g_fun.
+# Generate new exposures under user-specific arbitrary intervention f.g_fun.
+OData.gstar$make.dat.sVar(p = 1, f.g_fun = f.gstar) 
 dim(OData.gstar$dat.sVar)  # 10000     5
 OData.gstar$make.dat.sVar(p = 3, f.g_fun = f.gstar) 
 dim(OData.gstar$dat.sVar)  # 30000     5
