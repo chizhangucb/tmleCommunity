bf3eb8c050312ceeaba14e3ac5e9eda7dc2f61a0
chizhangucb <chi.zhang@berkeley.edu>
2017-08-28 15:35:04 -0700

    Update tmleCommunity.R

--- R/tmleCommunity.R
+++ R/tmleCommunity.R
@@ -476,8 +476,10 @@ tmleCommunity <- function(data, Ynode, Anodes, Wnodes, Enodes = NULL, YnodeDet =
  
   ## Check if any unexpected inputs
   community.step <- community.step[1]
-  if (!(community.step %in% c(NULL, "stratify", "panel.transform"))) 
-    stop("community.step argument must be one of 'stratify', 'panel.transform' or NULL")
+  if (!(is.null(community.step))) {
+    if (!(community.step %in% c("stratify", "panel.transform"))) 
+      stop("community.step argument must be one of NULL, 'stratify', and 'panel.transform' ")
+  }
   if (community.step == "panel.transform") {
     transData <- panelData.Trans(yvar = getopt("panel.yvar"), xvar = getopt("panel.xvar"), data = data, 
                             effect = getopt("panel.effect"), model = getopt("panel.model"), index = communityInd)
