4bac8c2944e0384647ecc4cbef098426f7a69c2d
chizhangucb <chi.zhang@berkeley.edu>
2017-09-25 23:37:38 -0700

    Update tests_05_clusterContA_tmle.R

--- tests/testthat/tests_05_clusterContA_tmle.R
+++ tests/testthat/tests_05_clusterContA_tmle.R
@@ -3,6 +3,7 @@
 # ---------------------------------------------------------------------------------
 # Fitting continuous exposure by logistic regression
 # ---------------------------------------------------------------------------------
+source("tmleCommunity/tests/dataGeneration/get.cluster.dat.Acont.R")
 library(testthat)
 library(Hmisc)
 gvars$verbose <- TRUE
@@ -14,11 +15,8 @@ gvars$verbose <- TRUE
 ######################################### 
 ## Test 1.1 speed.glm & glm 
 ######################################### 
-## Test 1.1.1 Correctly specified Qform & gform
-
-nReps <- 100
-J <- 100
-n.ind <- 100
+truncBD <- 5
+shift.val <- 1
 
 define_f.gstar <- function(shift.val, truncBD, rndseed = NULL) {
   shift.const <- shift.val
@@ -41,11 +39,9 @@ Qform.mis <- "Y ~ E1 + W2 + A" # incorrect Q
 gform.corr <- "A ~ E1 + E2 + W1 + W2 + W3"  # correct g
 gform.mis <- "A ~ E2 + W1 + W2"  # correct g
 
-bootstrap.TMLE <- function(nBoot, J, n.ind, nbins = 5, Qestimator = "speedglm__glm", gestimator = "speedglm__glm", 
+bootstrap.TMLE <- function(nBoot, J, n.ind, truncBD, shift.val, nbins = 5, Qestimator = "speedglm__glm", gestimator = "speedglm__glm", 
                            f.gstar, lbound = 0.005, working.model = TRUE, Qform = NULL, gform = NULL, rndseed = NULL) {
-  truncBD <- 5
-  shift.val <- 1
-  
+
   est_tmle1a <- est_tmle1b <- est_tmle2 <- est_tmlePer <- NULL
   var_tmle1a <- var_tmle1b <- var_tmle2 <- var_tmlePer <- NULL
   CI_tmle1a <- CI_tmle1b <- CI_tmle2 <- CI_tmlePer <- NULL
@@ -106,4 +102,61 @@ bootstrap.TMLE <- function(nBoot, J, n.ind, nbins = 5, Qestimator = "speedglm__g
               CI_tmle1a = CI_tmle1a, CI_tmle1b = CI_tmle1b, CI_tmle2 = CI_tmle2, CI_tmlePer = CI_tmlePer,
               pval_tmle1a = pval_tmle1a, pval_tmle1b = pval_tmle1b, pval_tmle2 = pval_tmle2, pval_tmlePer = pval_tmlePer))
 }
+
+## Test 1.1.1 Correctly specified Qform & gform
+## Test 1.1.1.1 The number of bootstraps = 100 & the number of communities = 50 & the number of individual per community = 100
+tmle.Qc.gc.J50.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.corr, rndseed = 1)
+save(tmle.Qc.gc.J50.n100.nReps100, file = "tmle.Qc.gc.J50.n100.nReps100.Rda")
+
+## Test 1.1.1.2 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 100
+tmle.Qc.gc.J200.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 200, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                                Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                                lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.corr, rndseed = 1)
+save(tmle.Qc.gc.J200.n100.nReps100, file = "tmle.Qc.gc.J200.n100.nReps100.Rda")
+
+## Test 1.1.1.3 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 200
+tmle.Qc.gc.J50.n200.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 200, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.corr, rndseed = 1)
+save(tmle.Qc.gc.J50.n200.nReps100, file = "tmle.Qc.gc.J50.n200.nReps100.Rda")
+
+## Test 1.1.2 Correctly specified Qform & Misspecified gform
+## Test 1.1.2.1 The number of bootstraps = 100 & the number of communities = 50 & the number of individual per community = 100
+tmle.Qc.gm.J50.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.mis, rndseed = 1)
+save(tmle.Qc.gm.J50.n100.nReps100, file = "tmle.Qc.gm.J50.n100.nReps100.Rda")
+
+## Test 1.1.2.2 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 100
+tmle.Qc.gm.J200.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 200, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                                Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                                lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.mis, rndseed = 1)
+save(tmle.Qc.gm.J200.n100.nReps100, file = "tmle.Qc.gm.J200.n100.nReps100.Rda")
+
+## Test 1.1.2.3 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 200
+tmle.Qc.gm.J50.n200.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 200, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.corr, gform = gform.mis, rndseed = 1)
+save(tmle.Qc.gm.J50.n200.nReps100, file = "tmle.Qc.gm.J50.n200.nReps100.Rda")
+
+## Test 1.1.3 Misspecified Qform & Correctly specified gform
+## Test 1.1.3.1 The number of bootstraps = 100 & the number of communities = 50 & the number of individual per community = 100
+tmle.Qm.gc.J50.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.mis, gform = gform.corr, rndseed = 1)
+save(tmle.Qm.gc.J50.n100.nReps100, file = "tmle.Qm.gc.J50.n100.nReps100.Rda")
+
+## Test 1.1.3.2 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 100
+tmle.Qm.gc.J200.n100.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 200, n.ind = 100, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                                Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                                lbound = 0.005, working.model = TRUE, Qform = Qform.mis, gform = gform.corr, rndseed = 1)
+save(tmle.Qm.gc.J200.n100.nReps100, file = "tmle.Qm.gc.J200.n100.nReps100.Rda")
+
+## Test 1.1.3.3 The number of bootstraps = 100 & the number of communities = 200 & the number of individual per community = 200
+tmle.Qm.gc.J50.n200.nReps100 <- bootstrap.TMLE(nBoot = 100, J = 50, n.ind = 200, truncBD = truncBD, shift.val = shift.val, nbins = 5,
+                                               Qestimator = "speedglm__glm", gestimator = "speedglm__glm", f.gstar = f.gstar, 
+                                               lbound = 0.005, working.model = TRUE, Qform = Qform.mis, gform = gform.corr, rndseed = 1)
+save(tmle.Qm.gc.J50.n200.nReps100, file = "tmle.Qm.gc.J50.n200.nReps100.Rda")
 
