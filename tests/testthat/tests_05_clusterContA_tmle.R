c90fabefd81d088a13897f36dbbf7980710ae671
chizhangucb <chi.zhang@berkeley.edu>
2017-09-25 21:43:46 -0700

    Create tests_05_clusterContA_tmle.R

---
+++ tests/testthat/tests_05_clusterContA_tmle.R
@@ -0,0 +1,7 @@
+# ---------------------------------------------------------------------------------
+# TEST SET 5. TESTS FOR FITTING CONTINUOUS EXPOSURE A IN HIERARCHICAL DATA
+# ---------------------------------------------------------------------------------
+# Fitting continuous exposure by logistic regression
+# ---------------------------------------------------------------------------------
+library(testthat)
+gvars$verbose <- TRUE
