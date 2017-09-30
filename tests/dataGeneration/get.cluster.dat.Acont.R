643ba6dd6349a8dff23e59a61741202cc0f2c09b
chizhangucb <chi.zhang@berkeley.edu>
2017-09-29 17:04:47 -0700

    Update get.cluster.dat.Acont.R

--- tests/dataGeneration/get.cluster.dat.Acont.R
+++ tests/dataGeneration/get.cluster.dat.Acont.R
@@ -14,7 +14,8 @@ get.cluster.dat.Acont <- function(id, n.ind = 1000, rndseed = NULL, is.Y.bin = T
   W1 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.4 + 1.2 * E1 - 1.3 * E2))
   UW2 <- MASS::mvrnorm(n = n.ind, mu = rep(0, 2), Sigma = matrix(c(1, 0.6, 0.6, 1), ncol = 2))
   W2 <- 0.8 * E1 - 0.4 * E2 + 1 * UW2[, 1] 
-  W3 <- sample(x = 1:12, size = n.ind, replace = TRUE) / 10
+  # W3 <- sample(x = 1:12, size = n.ind, replace = TRUE) / 10
+  W3 <- rbinom(n = n.ind, size = 1, prob = 0.5)
   
   # Construct continuous exposure; time-varying within community if timevarying = T
   if (timevarying) { # A is time-varying within community 
@@ -25,30 +26,29 @@ get.cluster.dat.Acont <- function(id, n.ind = 1000, rndseed = NULL, is.Y.bin = T
     A <- rnorm(n = 1, mean = A.mu, sd = 1)
   }
   untrunc.A.gstar <- A + shift.val
-  r.new.A <- exp(1.8 * shift.val * (untrunc.A.gstar - A.mu - shift.val / 4))
+  r.new.A <- exp(1.5 * shift.val * (untrunc.A.gstar - A.mu - shift.val / 4))
   trunc.A.gstar <- ifelse(r.new.A > truncBD, A, untrunc.A.gstar)
   
   # Construct outcome
   if (working.model) {  # when working.model holds
     if (is.Y.bin) {
-      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 1.7 + 3.4 * A + 0.5 * E1 - 1.2 * E2 + 0.8 * W1 + 1 * W2 - 0.1 * W3))
-      Y.gstar <- rbinom(n = n.ind, size = 1, prob = plogis(- 1.7 + 3.4 * trunc.A.gstar + 0.5 * E1 - 1.2 * E2 + 0.8 * W1 + 1 * W2 - 0.1 * W3))
+      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 1.7 + 0.6 * A + 0.5 * E1 - 1.2 * E2 + 0.7 * W1 + 1.3 * W2 - 0.4 * W3))
+      Y.gstar <- rbinom(n = n.ind, size = 1, prob = plogis(- 1.7 + 0.6 * trunc.A.gstar + 0.5 * E1 - 1.2 * E2 + 0.7 * W1 + 1.3 * W2 - 0.4 * W3))
     } else {
       # finishing ....
     }
   } else {  # when working.model fails
     if (is.Y.bin) {
-      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 5 + 1 * A - 0.2 * E1 + 1.1 * E2 - 0.4 * W1 + 0.2 * W2 - 0.3 * W3 + 
+      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 5 + 1 * A - 0.2 * E1 + 1.1 * E2 - 0.4 * W1 + 0.2 * W2 - 0.4 * W3 + 
                                                        5.8 * mean(W1) + 3.1 * mean(W2) - 1 * mean(W3)))
       Y.gstar <- rbinom(n = n.ind, size = 1, prob = plogis(- 5 + 1 * trunc.A.gstar - 0.2 * E1 + 1.1 * E2 - 0.4 * W1 + 0.2 * W2 - 
-                                                             0.3 * W3 + 5.8 * mean(W1) + 3.1 * mean(W2) - 1 * mean(W3)))
+                                                             0.4 * W3 + 5.8 * mean(W1) + 3.1 * mean(W2) - 1 * mean(W3)))
     } else {
       # finishing ....
     }
   }
   return(data.frame(cbind(id, E1, E2, W1, W2, W3, A, trunc.A.gstar, Y, Y.gstar)))
 }
-
 
 get.fullDat.Acont <- function(J, n.ind, rndseed = NULL, is.Y.bin = TRUE, truncBD = 5, shift.val = 1, working.model = TRUE, 
                               timevarying = TRUE, n.ind.fix = FALSE, onlyYkeep = FALSE, verbose = TRUE) {
