9c2ebcf2d7469a1fab0e84f7b07dd76213b8a82e
chizhangucb <chi.zhang@berkeley.edu>
2017-11-02 22:05:16 -0700

    Update tmleCommunity.R

--- R/tmleCommunity.R
+++ R/tmleCommunity.R
@@ -490,13 +490,13 @@ CalcAllEstimators <- function(OData.ObsP0, est_params_list) {
 #'     the common (in j) conditional distribution of \eqn{A^*} given \eqn{(W, E)}, that is \eqn{P_{g_0^{c*}}(A^* | W, E)} for common (in j)
 #'     community-level covariates (\eqn{A^*, W, E}) where \eqn{A^*} is determined by the user-supplied stochastic intervention \code{f_gstar1} or 
 #'     \code{f_gstar2}, given the observed baseline covariates \eqn{(W, E)}. 
-#'   \item Given observed J independent communities \eqn{\textbf{O}_j = (E_j, \textbf{W}_j, A_j, Y^c_j: j = 1, ..., J)}, the IPTW estimator is given by
+#'   \item Given observed J independent communities \eqn{\textbf{O}_j = (E_j, \textbf{W}_j, A_j, Y^c_j: j = 1, ..., J)}, the IPTW estimator is given by:
 #'     \deqn{\psi^{I}_{IPTW, n}=\frac{1}{J}\sum_{j=1}^{J}Y^c_j\frac{P_{\hat{g}^{c*}}(A^*_j|\textbf{W}_j,E_j)}{P_{\hat{g}^{c*}}(A_j|\textbf{W}_j,E_j)}}
 #'  }
 #'
 #' For individual-level IPTW, it read the input data as \eqn{O_{i,j} = (E_j, W_{i,j}, A_j, Y_{i,j}: j = 1, ..., J; i = 1, ..., n_j)} and incorporates 
 #'  working model that assumes no covariate interference, weighing each individual within one community by \eqn{\alpha_{i,j}}, where the IPTW estimator
-#'  is given by \deqn{\psi^{II}_{IPTW, n}=\frac{1}{J}\sum_{j=1}^{J}\sum_{i=1}^{n_j}\alpha_{i,j}Y_{i,j}\frac{P_{g^{*}}(A^*_j|W_{i,j}, E_j)}
+#'  is given by: \deqn{\psi^{II}_{IPTW, n}=\frac{1}{J}\sum_{j=1}^{J}\sum_{i=1}^{n_j}\alpha_{i,j}Y_{i,j}\frac{P_{g^{*}}(A^*_j|W_{i,j}, E_j)}
 #'   {P_{g^{*}}(A_j | W_{i,j}, E_j)}}
 #' 
 #' @section TMLE estimator:
@@ -508,9 +508,12 @@ CalcAllEstimators <- function(OData.ObsP0, est_params_list) {
 #'   \item Construct an initial estimator \eqn{\hat{\bar{Q}}^c(A | W, E)} of the common (in j) conditional distribution of \eqn{Y^c} given 
 #'     \eqn{(A, W, E)} and update \eqn{\hat{\bar{Q}^{c*}}^c(A | W, E)} for \eqn{\hat{\bar{Q}}^c(A | W, E)} by weights calculated in the first step.
 #'   \item The TMLE estimator is defined as the following substitution estimator:
-#'     \deqn{\psi^{I}_{TMLE, n}=\frac{1}{J}\sum_{j=1}^{J}Y^c_j\int_{a}\hat{\bar{Q}}^{c*}(a, \textbf{W}_j, E_j)g^{c*}(a|\textbf{W}_j, E_j)d\mu(a)}
+#'     \deqn{\psi^{I}_{TMLE, n}=\frac{1}{J}\sum_{j=1}^{J}\int_{a}\hat{\bar{Q}}^{c*}(a, \textbf{W}_j, E_j)g^{c*}(a|\textbf{W}_j, E_j)d\mu(a)}
 #'  }
 #'
+#' For individual-level TMLE, its estimator is given by:
+#'  \deqn{\psi^{II}_{TMLE,n}=\frac{1}{J}\sum_{j=1}^{J}\sum_{i=1}^{n_j}\alpha_{i,j}\int_{a}\hat{\bar{Q}}^*(a, w_{i,j}, E_j)g^*(a|w_{i,j}, E_j)d\mu(a)}
+#' 
 #' @section GCOMP estimator:
 #' 
 #' 
