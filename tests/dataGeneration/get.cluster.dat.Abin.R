39b87028561061e6249b2a3cbcf2f6d57df4c6ff
chizhangucb <chi.zhang@berkeley.edu>
2017-11-04 10:35:57 -0700

    Update get.cluster.dat.Abin.R

--- tests/dataGeneration/get.cluster.dat.Abin.R
+++ tests/dataGeneration/get.cluster.dat.Abin.R
@@ -5,33 +5,81 @@
 # individual exposure is normal with mu for each observation being a function of (E1, E2, W1, W2);
 # -------------------------------------------------------------------------------------------------------
 
-get.cluster.dat.Abin <- function(id, n.incluster = 10000, rndseed = NULL, is.Y.bin = T, working.model = T, timevarying = F) {
+get.cluster.dat.Abin <- function(id, n.ind = 10000, rndseed = NULL, is.Y.bin = T, working.model = T) {
   set.seed(rndseed)
   E1 <- runif(n = 1, min = 0, max = 1)
-  E2 <- sample(x = c(0, 0.5, 1), size = 1)
-  W1 <- rbinom(n = n.incluster, size = 1, prob = plogis(- 0.7 + 1.2 * E1 - 2 * E2))
-  UW2 <- MASS::mvrnorm(n = n.incluster, mu = rep(0, 2), Sigma = matrix(c(1, 0.7, 0.7, 1), ncol = 2))
-  W2 <- 0.8 * E1 + 0.6 * E2 + 1 * UW2[, 1] # + 1 * UW2[, 2]
-  if (timevarying) {
-    A <- rbinom(n = 1, size = 1, prob = plogis(- 2.8 + 3.5 * E1 + 0.4 * E2 + 6 * mean(W1)))  # community-level A
-  }
+  E2 <- sample(x = c(0, 0.4, 0.8, 1), size = 1)
+  W1 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.23 + 1.2 * E1 -  1.8 * E2))
+  W2_mean <- - 0.6 + 0.8 * E1 - 0.4 * E2 
+  W3_mean <- 0.5 + 0.2 * E1
+  W2W3 <- MASS::mvrnorm(n = n.ind, mu = c(W2_mean, W3_mean), Sigma = matrix(c(1, 0.6, 0.6, 1), ncol = 2))
+  W2 <- W2W3[, 1]
+  W3 <- W2W3[, 2] 
+  A <- rbinom(n = 1, size = 1, prob = plogis(- 2.3 + 3 * E1 + 0.4 * E2 + 5 * mean(W1)))  # community-level A
   if (working.model) {  # working.model holds
-    if (is.Y.bin) {
-      Y <- rbinom(n = n.incluster, size = 1, prob = plogis(- 2 + 2 * A + 2 * E1 + 0.6 * E2 + W1 + 0.6 * W2))
-      Y1 <- rbinom(n = n.incluster, size = 1, prob = plogis(- 2 + 2 * 1 + 2 * E1 + 0.6 * E2 + W1 + 0.6 * W2))
-      Y0 <- rbinom(n = n.incluster, size = 1, prob = plogis(- 2 + 2 * 0 + 2 * E1 + 0.6 * E2 + W1 + 0.6 * W2))
-    } else {
-      # finishing ....
+    if (is.Y.bin) {  # Y binary
+      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * A + 0.5 * E1 + 0.2 * E2 + W2 - 0.2 * W3))
+      Y1 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * 1 + 0.5 * E1 + 0.2 * E2 + W2 - 0.2 * W3))
+      Y0 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * 0 + 0.5 * E1 + 0.2 * E2 + W2 - 0.2 * W3))
+    } else {  # Y continuous 
+      Y <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.4 * W2 + 0.2 * W3, sd = 1)
+      Y1 <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.4 * W2 + 0.2 * W3, sd = 1)
+      Y0 <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.4 * W2 + 0.2 * W3, sd = 1)
     }
   } else {  # working.model fails
-    if (is.Y.bin) {
-      Y <- rbinom(n = n.incluster, size = 1, prob = plogis(-3 + 1 * A + 0.2 * E1 + 0.2 * E2 + 0.1 * W1 + 0.2 * W2 + 3 * mean(W1) + 1.5 * mean(W2)))
-      Y1 <- rbinom(n = n.incluster, size = 1, prob = plogis(-3 + 1 * 1 + 0.2 * E1 + 0.2 * E2 + 0.1 * W1 + 0.2 * W2 + 3 * mean(W1) + 1.5 * mean(W2)))
-      Y0 <- rbinom(n = n.incluster, size = 1, prob = plogis(-3 + 1 * 0 + 0.2 * E1 + 0.2 * E2 + 0.1 * W1 + 0.2 * W2 + 3 * mean(W1) + 1.5 * mean(W2)))
-    } else {
-      # finishing ....
+    if (is.Y.bin) {  # Y binary
+      Y <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * A + 0.5 * E1 + 0.2 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3)))
+      Y1 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * 1 + 0.5 * E1 + 0.2 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3)))
+      Y0 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.2 + 0.3 * 0 + 0.5 * E1 + 0.2 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3)))
+    } else {  # Y continuous 
+      Y <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3), sd = 1)
+      Y1 <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3), sd = 1)
+      Y0 <- rnorm(n = n.ind, mean = 1 + 0.3 * A - 0.2 * E1 + 0.4 * E2 + 0.3 * W1 - 0.15 * W3 + 0.75 * mean(W2) + 0.4 * mean(W3), sd = 1)
     }
   }
-  return(data.frame(cbind(id, E1, E2, W1, W2, A, Y, Y1, Y0)))
+  return(data.frame(cbind(id, E1, E2, W1, W2, W3, A, Y, Y1, Y0)))
 }
 
+get.fullDat.Abin <- function(J, n.ind, rndseed = NULL, is.Y.bin = TRUE, working.model = TRUE, 
+                             n.ind.fix = FALSE, onlyYkeep = FALSE, verbose = TRUE) {
+  set.seed(rndseed)
+  if (n.ind.fix) {
+    n.ind <- rep(n.ind, J)
+  } else {  # don't fix the number of individuals in each community
+    n.ind <- round(rnorm(J, n.ind, 10))  
+    n.ind[n.ind <= 0] <- n.ind  # set to n.ind if any generated number less than 1
+  }
+  
+  if (onlyYkeep) {
+    message("Only the observed & (two) post-intervened outcomes are useful, only just keep these three")
+    Y <- Y1 <- Y0 <- NULL
+  } else {
+    full.data <- NULL
+  }
+  
+  for(j in 1:J) {
+    if (verbose) message("#### generating " %+% j %+% "th cluster ####")
+    cluster.data.j <- get.cluster.dat.Abin(id = j, n.ind = n.ind[j], rndseed = rndseed, is.Y.bin = is.Y.bin, working.model = working.model) 
+    if (onlyYkeep) {
+      Y <- c(Y, cluster.data.j[, "Y"])
+      Y1 <- c(Y.gstar, cluster.data.j[, "Y1"])
+      Y0 <- c(Y.gstar, cluster.data.j[, "Y0"])
+    } else {
+      full.data <- rbind(full.data, cluster.data.j)
+    }
+  }  
+  ifelse(onlyYkeep, return(data.frame(cbind(Y, Y1, Y0))), return(full.data))
+}
+
+J <- 1000
+n.ind <- 100
+rndseed <- 12345
+
+#### Data 1. One binary, community-level A with Binary Y, when working model holds
+comPop.wmT.bA.bY <- get.fullDat.Abin(J = 4000, n.ind = 4000, rndseed = NULL, is.Y.bin = T, working.model = T, n.ind.fix = T)
+mean(comPop.wmT.bA.bY$Y1) # 
+mean(comPop.wmT.bA.bY$Y0) # 
+psi0.Y <- mean(comPop.wmT.bA.bY$Y1) - mean(comPop.wmT.bA.bY$Y0) # 
+
+comSample.wmT.bA.bY <- get.fullDat.Abin(J = J, n.ind = n.ind, rndseed = rndseed, is.Y.bin = T, working.model = T, n.ind.fix = T)
+comSample.wmT.bA.bY_list <- list(comSample.wmT.bA.bY = comSample.wmT.bA.bY, psi0.Y = psi0.Y, rndseed = rndseed)
