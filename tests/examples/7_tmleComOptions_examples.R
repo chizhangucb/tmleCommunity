24e58122fd8c7b94fc82d9a441f4eb2ca8fdc1d8
chizhangucb <chi.zhang@berkeley.edu>
2017-11-05 15:21:53 -0800

    Update 7_tmleComOptions_examples.R

--- tests/examples/7_tmleComOptions_examples.R
+++ tests/examples/7_tmleComOptions_examples.R
@@ -1,10 +1,26 @@
-tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm", nbins = 20)
+#***************************************************************************************
+# Example 1: using different estimators in estimation of Q and g mechanisms
+#***************************************************************************************
+# 1.1 speed.glm (and glm)
+tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm")
+tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "glm__glm")
+
+# 1.2 SuperLearner
+# library including "SL.glm", "SL.glmnet", "SL.ridge", and "SL.stepAIC"
+tmleCom_Options(Qestimator = "SuperLearner", gestimator = "SuperLearner", maxNperBin = nrow(data),
+                SL.library = c("SL.glm", "SL.glmnet", "SL.ridge", "SL.stepAIC"), CVfolds = 5)
+
+# library including "SL.glm", "SL.glmnet", "SL.gam", and "SL.randomForest"
+tmleCom_Options(Qestimator = "SuperLearner", gestimator = "SuperLearner", maxNperBin = nrow(data),
+                SL.library = c("SL.glm", "SL.glmnet", "SL.gam", "SL.randomForest"), CVfolds = 10)
+
+# Create custom learners (e.g., a sequence of learners with hyperparameter combinations)
+
 
 tmleCom_Options(Qestimator = "h2o__ensemble", gestimator = "h2o__ensemble", maxNperBin = 10000,
                 h2olearner = c("h2o.glm.wrapper", "h2o.randomForest.wrapper"), CVfolds = 10)
                 
-tmleCom_Options(Qestimator = "SuperLearner", gestimator = "SuperLearner", maxNperBin = nrow(data),
-                SL.library = c("SL.glm", "SL.glmnet", "SL.ridge", "SL.stepAIC"), CVfolds = 5)
+
 
 tmleCom_Options(Qestimator = "SuperLearner", gestimator = "h2o__ensemble", maxNperBin = nrow(data),
                 SL.library = c("SL.glm", "SL.glmnet", "SL.ridge", "SL.stepAIC"), CVfolds = 5,
