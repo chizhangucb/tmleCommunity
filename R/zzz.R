24e9830334fffe1670867284659e1896f2e9277a
chizhangucb <chi.zhang@berkeley.edu>
2017-11-05 11:59:45 -0800

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -95,20 +95,7 @@ print_tmleCom_opts <- function() {
 #' 
 #' @seealso \code{\link{print_tmlenet_opts}}
 #' 
-#' @examples
-#' tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm", nbins = 20)
-#'
-#' tmleCom_Options(Qestimator = "h2o__ensemble", gestimator = "h2o__ensemble", maxNperBin = 10000,
-#'                     h2olearner = c("h2o.glm.wrapper", "h2o.randomForest.wrapper"), CVfolds = 10)
-#'
-#' tmleCom_Options(Qestimator = "SuperLearner", gestimator = "SuperLearner", maxNperBin = nrow(data),
-#'                     SL.library = c("SL.glm", "SL.glmnet", "SL.ridge", "SL.stepAIC"), CVfolds = 5)
-#' 
-#' tmleCom_Options(Qestimator = "SuperLearner", gestimator = "h2o__ensemble", maxNperBin = nrow(data),
-#'                     SL.library = c("SL.glm", "SL.glmnet", "SL.ridge", "SL.stepAIC"), CVfolds = 5,
-#'                 h2ometalearner = "h2o.deeplearning.wrapper", 
-#'                 h2olearner = c("h2o.gbm.wrapper", "h2o.randomForest.wrapper"))
-#' 
+#' @examples tests/examples/7_tmleComOptions_examples.R
 #' @export
 tmleCom_Options <- function(Qestimator = c("speedglm__glm", "glm__glm", "h2o__ensemble", "SuperLearner"),  
                             gestimator = c("speedglm__glm", "glm__glm", "h2o__ensemble", "SuperLearner"),  
