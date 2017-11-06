be4e71755394c971371ec90ae691a12ad22f178e
chizhangucb <chi.zhang@berkeley.edu>
2017-11-05 23:36:31 -0800

    Update tmleCommunity.R

--- R/tmleCommunity.R
+++ R/tmleCommunity.R
@@ -684,7 +684,7 @@ tmleCommunity <- function(data, Ynode, Anodes, WEnodes, YnodeDet = NULL, obs.wts
   } else if (obs.wts == "equal.within.community") { # weigh individuals in each community equally and weigh communities equally
     obs.wts <- rep(as.vector(1/table(data[, communityID])), as.vector(table(data[, communityID])))
   }
-  jf (is.character(community.step)) community.step <- community.step[1]
+  if (is.character(community.step)) community.step <- community.step[1]
   if (is.character(community.step) && (community.step != "NoCommunity") && !is.data.frame(community.wts)) {
     community.wts.df <- as.data.frame(matrix(0L, nrow = length(unique(data[, communityID])), ncol = 2))
     colnames(community.wts.df) <- c("id", "weights")
