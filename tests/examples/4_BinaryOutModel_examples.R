44f2d131f80078731fb2da70c827ff633b35d79b
chizhangucb <chi.zhang@berkeley.edu>
2017-11-07 08:50:47 -0800

    Update 4_BinaryOutModel_examples.R

--- tests/examples/4_BinaryOutModel_examples.R
+++ tests/examples/4_BinaryOutModel_examples.R
@@ -1,17 +1,18 @@
-### Load data
-# A is normal with mu for each observation being a function of (W1, W2, W3, W4), sd = 1;
-data(sampleDat_iidcontABinY)
-dat_iidcontABinY <- sampleDat_iidcontABinY$dat_iidcontABinY
-nodes <- list(Ynode = "Y", Anodes = "A", Wnodes = c("W1", "W2", "W3", "W4"), Enodes = NULL, Crossnodes = NULL)
-gvars$verbose <- TRUE  # Print status messages
+#***************************************************************************************
+### Load data: A is normal with mu for each observation being a function of (W1, W2, W3, W4)
+data(indSample.iid.cA.bY_list)
+indSample.iid.cA.bY <- indSample.iid.cA.bY_list$indSample.iid.cA.bY
+nodes <- list(Ynode = "Y", Anodes = "A", WEnodes = c("W1", "W2", "W3", "W4"), Crossnodes = NULL)
+verbose <- TRUE  # Print status messages
 
-# First, try to use Speedglm to fit regression (GLMs to medium-large datasets)
-tmleCom_Options(Qestimator = "speedglm__glm", maxNperBin = nrow(dat_iidcontABinY))
-OData <- DatKeepClass$new(Odata = dat_iidcontABinY, nodes = nodes, norm.c.sVars = FALSE)
+# 1. Speedglm to fit regression (it's GLMs to medium-large datasets)
+tmleCom_Options(Qestimator = "speedglm__glm", maxNperBin = nrow(indSample.iid.cA.bY))
+OData <- DatKeepClass$new(Odata = indSample.iid.cA.bY, nodes = nodes, norm.c.sVars = FALSE)
 Q.sVars <- define_regform(NULL, Anodes.lst = nodes$Ynode, 
                           Wnodes.lst = nodes[c("Anodes", "Wnodes", "Enodes")])
 Qreg <- RegressionClass$new(outvar = Q.sVars$outvars, predvars = Q.sVars$predvars, 
                             subset_vars = (!rep_len(FALSE, nrow(dat_iidcontABinY))))
+
 # By setting savespace = FALSE, save all data in fit step
 m.Q.init <- BinaryOutModel$new(reg = Qreg)$fit(overwrite = FALSE, data = OData, savespace = FALSE)
 m.Q.init$is.fitted  # TRUE
@@ -27,6 +28,7 @@ library(SuperLearner)
 Qreg$estimator <- "SuperLearner"
 gvars$opts$g.SL.library <- c("SL.glm", "SL.step", "SL.glm.interaction", "SL.randomForest")
 set.seed(1)
+
 # To save (RAM) space, wipe out all internal data when doing many stacked fitting regressions
 m.Q.init <- BinaryOutModel$new(reg = Qreg)$fit(overwrite = FALSE, data = OData, savespace = TRUE)
 m.Q.init$predict(newdata = OData, savespace = TRUE)
