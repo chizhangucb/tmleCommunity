2279c8f5a90802b9851df04dd22bfb9c262608e1
chizhangucb <chi.zhang@berkeley.edu>
2017-11-07 23:56:58 -0800

    Update 2_DatKeepClass_examples.R

--- tests/examples/2_DatKeepClass_examples.R
+++ tests/examples/2_DatKeepClass_examples.R
@@ -9,7 +9,7 @@ tmleCom_Options(nbins = 10, maxNperBin = nrow(indSample.iid.cA.bY))
 #***************************************************************************************
 
 #***************************************************************************************
-# 1.1 Specifying the stochastic intervention of interest
+# 1.1 Specifying the stochastic intervention of interest gstar
 #***************************************************************************************
 # Interested in the effect of a shift of delta(W1, W3, W4) of the current treatment
 define_f.gstar <- function(data, ...) {
@@ -22,34 +22,37 @@ define_f.gstar <- function(data, ...) {
 # 1.2 Creating an R6 object of DatKeepClass (to store the input data)
 #***************************************************************************************
 # Don't normalize continous covariates by setting norm.c.sVars = FALSE
-OData <- DatKeepClass$new(Odata = indSample.iid.cA.bY, nodes = nodes, norm.c.sVars = FALSE)  
+OData_R6 <- DatKeepClass$new(Odata = indSample.iid.cA.bY, 
+                             nodes = nodes, norm.c.sVars = FALSE)  
 # names of all variables that are in input data and specified in nodes
-OData$names.sVar  # "Y"  "A"  "W1" "W3" "W4" 
+OData_R6$names.sVar  # "Y"  "A"  "W1" "W3" "W4" 
 # names of all continuous variables that are in input data and specified in nodes
-OData$names.c.sVar  # "A"  "W3" "W4" 
+OData_R6$names.c.sVar  # "A"  "W3" "W4" 
 # a sub dataframe of the input data, including all variables in nodes
-head(OData$dat.sVar) 
+head(OData_R6$dat.sVar) 
 # the number of observations of the input data
-OData$nobs  # 10000
-OData$get.sVar.type("A")  # "contin"
-OData$get.sVar.type()  # Provide a list of types of all variables
+OData_R6$nobs  # 10000
+OData_R6$get.sVar.type("A")  # "contin"
+OData_R6$get.sVar.type()  # Provide a list of types of all variables
 
 #***************************************************************************************
 # 1.3 Manipulating the input data by adding observed outcome and observation weights
 #***************************************************************************************
-OData$addYnode(YnodeVals = indSample.iid.cA.bY[, nodes$Ynode], det.Y = FALSE)  
-head(OData$YnodeVals)  # Adding public YnodeVals & setting det.Y values to NA
-head(OData$noNA.Ynodevals)  # Adding actual observed Y as protected (without NAs)
-OData$addObsWeights(obs.wts = rep(c(1,2), 5000))  # Add a vectopr of observation (sampling) weights
-OData$addObsWeights(obs.wts = 1)  # If not specified, assumed to be all 1 (i.e., equally weighted)
+# Add YnodeVals (a vector of outcomes) to both public and private field 
+OData_R6$addYnode(YnodeVals = indSample.iid.cA.bY[, nodes$Ynode], det.Y = FALSE)  
+head(OData_R6$YnodeVals)  # set YnodeVals[det.Y=TRUE] to NA in public field
+head(OData_R6$noNA.Ynodevals)  # YnodeVals in private field is protected (without NAs)  
+OData_R6$addObsWeights(obs.wts = rep(c(1,2), 5000))  # Add a vector of sampling weights
+ODaOData_R6ta$addObsWeights(obs.wts = 1)  # Assumed to be all 1 (i.e., equally weighted)
 
 #***************************************************************************************
-# 1.4 
+# 1.4 Creating an new R6 object of DatKeepClass under stochastic intervention g.star
+# Generate new exposures under user-specific arbitrary intervention f.g_fun
 #***************************************************************************************
-OData.gstar <- DatKeepClass$new(Odata = indSample.iid.cA.bY, nodes = nodes, norm.c.sVars = FALSE)
-# Create (p=) 1 new Odata (nobs obs at a time) and replace A under g0 in Odata with A^* under g.star
-# Generate new exposures under user-specific arbitrary intervention f.g_fun.
-OData.gstar$make.dat.sVar(p = 1, f.g_fun = define_f.gstar) 
-dim(OData.gstar$dat.sVar)  # 10000     4
-OData.gstar$make.dat.sVar(p = 3, f.g_fun = define_f.gstar) 
-dim(OData.gstar$dat.sVar)  # 30000     4
+OData.gstar_R6 <- DatKeepClass$new(Odata = indSample.iid.cA.bY, nodes = nodes)
+# Create 1 new Odata and replace A under g0 in Odata with A* under g.star 
+OData.gstar_R6$make.dat.sVar(p = 1, f.g_fun = define_f.gstar) 
+dim(OData.gstar_R6$dat.sVar)  # 10000     4
+# Create 3 new Odatas and repalce A with A*
+OData.gstar_R6$make.dat.sVar(p = 3, f.g_fun = define_f.gstar) 
+dim(OData.gstar_R6$dat.sVar)  # 30000     4
