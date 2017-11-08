701eb802e8d3b91981b6057165efcc85eec482b0
chizhangucb <chi.zhang@berkeley.edu>
2017-11-08 10:36:52 -0800

    Update 2_DatKeepClass_examples.R

--- tests/examples/2_DatKeepClass_examples.R
+++ tests/examples/2_DatKeepClass_examples.R
@@ -1,11 +1,11 @@
 #***************************************************************************************
 # Example 1: storing, managing, subsetting and manipulating a data with continuous A
 data(indSample.iid.cA.cY_list)
-indSample.iid.cA.bY <- indSample.iid.cA.bY_list$indSample.iid.cA.bY
-psi0.Y <- indSample.iid.cA.bY_list$psi0.Y  # 0.333676
+indSample.iid.cA.cY <- indSample.iid.cA.cY_list$indSample.iid.cA.cY
+psi0.Y <- indSample.iid.cA.cY_list$psi0.Y  # 0.333676
 # Assume that W2 has no effect on neither A nor Y, so no need to put into nodes
 nodes <- list(Ynode = "Y", Anodes = "A", WEnodes = c("W1", "W3", "W4"))  
-tmleCom_Options(nbins = 10, maxNperBin = nrow(indSample.iid.cA.bY))
+tmleCom_Options(nbins = 10, maxNperBin = nrow(indSample.iid.cA.cY))
 #***************************************************************************************
 
 #***************************************************************************************
@@ -23,7 +23,7 @@ define_f.gstar <- function(data, ...) {
 # 1.2 Creating an R6 object of DatKeepClass (to store the input data)
 #***************************************************************************************
 # Don't normalize continous covariates by setting norm.c.sVars = FALSE
-OData_R6 <- DatKeepClass$new(Odata = subset(indSample.iid.cA.bY, select=-Y), 
+OData_R6 <- DatKeepClass$new(Odata = subset(indSample.iid.cA.cY, select=-Y), 
                              nodes = nodes[c("Anodes", "WEnodes")], norm.c.sVars = FALSE)  
 # names of all variables that are in input data and specified in nodes
 OData_R6$names.sVar  # "A"  "W1" "W3" "W4" 
@@ -39,12 +39,18 @@ OData_R6$get.sVar.type()  # Provide a list of types of all variables
 #***************************************************************************************
 # 1.3 Manipulating the input data by adding observed outcomes and observation weights
 #***************************************************************************************
+# Bound observed outcome into [0, 1]
+obsYvals <- indSample.iid.cA.cY[, nodes$Ynode]
+ab <- range(obsYvals, na.rm=TRUE)
+indSample.iid.cA.cY[, nodes$Ynode] <- (obsYvals-ab[1]) / diff(ab)
+
 # Add YnodeVals (a vector of outcomes) to both public and private field 
-OData_R6$addYnode(YnodeVals = indSample.iid.cA.bY[, nodes$Ynode], det.Y = FALSE)  
+OData_R6$addYnode(YnodeVals = indSample.iid.cA.cY[, nodes$Ynode], det.Y = FALSE)  
 # set YnodeVals[det.Y=TRUE] to NA in public field (with NAs)
 head(OData_R6$YnodeVals)  
 # protect YnodeVals from being set to NA in private field (without NAs)  
 head(OData_R6$noNA.Ynodevals)  
+
 # Add a vector of observation (sampling) weights
 OData_R6$addObsWeights(obs.wts = rep(c(1,2), 5000))  
 # Assume all weights to be 1 (i.e., equally weighted)
