b0e221fc3ba3dfcec1c81d4cbb681836c7c9e61f
chizhangucb <chi.zhang@berkeley.edu>
2017-11-08 13:39:18 -0800

    Update and rename 4_RegressionClass_examples.R to 5_RegressionClass_examples.R

---
+++ tests/examples/5_RegressionClass_examples.R
@@ -0,0 +1,21 @@
+data(indSample.iid.cA.bY_list)
+indSample.iid.cA.bY <- indSample.iid.cA.bY_list$indSample.iid.cA.bY
+nodes <- list(Ynode = "Y", Anodes = "A", WEnodes = c("W1", "W2", "W3", "W4"))
+tmleCom_Options(maxNperBin = nrow(indSample.iid.cA.bY))
+
+OData.g0 <- DatKeepClass$new(Odata = indSample.iid.cA.bY, nodes = nodes)
+h.g0.sVars <- define_regform(A ~ W1 + W2 + W3 + W4) 
+A.nms.g0 <- h.g0.sVars$outvars
+regclass.g0 <- RegressionClass$new(outvar = A.nms.g0, predvars = h.g0.sVars$predvars,
+                                   subset_vars = lapply(A.nms.g0, function(var) {var}),
+                                   outvar.class = OData.g0$type.sVar[A.nms.g0])
+regclass.g0$estimator  # "speedglm__glm"
+regclass.g0$pool_cont  # FALSE (Don't pool across all bins of a continuous exposure)
+regclass.g0$parfit     # FALSE (Don't preform parallel computing in estimation)
+
+# Clone the parent regclass.g0 and reset to a single univariate k_i regression 
+# for outcome regclass.g0$outvar[[k_i]]
+k_i <- 1
+reg_i <- regclass.g0$clone()
+reg_i$ChangeManyToOneRegresssion(k_i, regclass.g0)
+genericmodels.g0.A1 <- ContinModel$new(reg = reg_i, DatKeepClass.g0 = OData.g0)
