e2e5037fc04feec6fad1ee3b98bfc7f05b64faf9
chizhangucb <chi.zhang@berkeley.edu>
2017-11-08 11:53:22 -0800

    Update 3_fitGenericDensity_examples.R

--- tests/examples/3_fitGenericDensity_examples.R
+++ tests/examples/3_fitGenericDensity_examples.R
@@ -1,35 +1,20 @@
-#***************************************************************************************
-# Example 1: 
-#***************************************************************************************
-data(sampleDat_iidcontABinY)
-dat_iidcontABinY <- sampleDat_iidcontABinY$dat_iidcontABinY
-psi0.Y <- sampleDat_iidcontABinY$psi0.Y  # 0.291398
-psi0.Ygstar <- sampleDat_iidcontABinY$psi0.Ygstar  # 0.316274
+data(indSample.iid.cA.bY_list)
+indSample.iid.cA.bY <- indSample.iid.cA.bY_list$indSample.iid.cA.bY
+tmleCom_Options(gestimator = "speedglm__glm", maxNperBin = nrow(indSample.iid.cA.bY))
+gvars$verbose <- TRUE  # Print status messages (global setting)
 
-### Define a stochastic intervention
-define_f.gstar <- function(shift.val, truncBD, rndseed = NULL) {
-  shift.const <- shift.val
-  trunc.const <- truncBD
-  f.gstar <- function(data, ...) {
-    print("shift.const: " %+% shift.const)
-    set.seed(rndseed)
-    A.mu <- 0.86 * data[,"W1"] + 0.41 * data[,"W2"] - 0.34 * data[,"W3"] + 0.93 * data[,"W4"]
-    untrunc.A <- rnorm(n = nrow(data), mean = A.mu + shift.const, sd = 1)
-    r.new.A <- exp(0.8 * shift.const * (untrunc.A - A.mu - shift.const / 3))
-    trunc.A <- ifelse(r.new.A > trunc.const, untrunc.A - shift.const, untrunc.A)
-    return(trunc.A)
-  }
-  return(f.gstar)
+# Define a stochastic intervention
+define_f.gstar <- function(data, shift.rate, ...) {
+  print("rate of shift: " %+% shift.rate)
+  shifted.new.A <- data[, "A"] * shift.rate
+  return(shifted.new.A)
 }
-f.gstar <- define_f.gstar(shift = sampleDat_iidcontABinY$shift.val, 
-                          truncBD = sampleDat_iidcontABinY$truncBD, 
-                          rndseed = sampleDat_iidcontABinY$rndseed)
+f.gstar <- define_f.gstar(shift.rate = 0.5)
 
-### Run
-tmleCom_Options(maxNperBin = nrow(dat_iidcontABinY))
-h_gN <- fitGenericDensity(data = dat_iidcontABinY, Anodes = "A", 
+# Run
+h_gN <- fitGenericDensity(data = indSample.iid.cA.bY, Anodes = "A", 
                           Wnodes = c("W1", "W2", "W3", "W4"), 
                           f_gstar = NULL, lbound = 0)$h_gstar
-h_gstar <- fitGenericDensity(data = dat_iidcontABinY, Anodes = "A",
+h_gstar <- fitGenericDensity(data = indSample.iid.cA.bY, Anodes = "A",
                              Wnodes = c("W1", "W2", "W3", "W4"), 
                              f_gstar = f.gstar, lbound = 0)$h_gstar
