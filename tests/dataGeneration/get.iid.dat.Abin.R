f411f7bc0e170a6e6121c78e24867e3b868f6cf0
chizhangucb <chi.zhang@berkeley.edu>
2017-11-08 21:34:17 -0800

    Update get.iid.dat.Abin.R

--- tests/dataGeneration/get.iid.dat.Abin.R
+++ tests/dataGeneration/get.iid.dat.Abin.R
@@ -5,6 +5,9 @@
 # individual exposure is normal with mu for each observation being a function of (W1, W2, W3, W4), sd = 1;
 # ---------------------------------------------------------------------------------
 
+#**************************************************************
+# FUNCTION 1. Non-rare outcome 
+#**************************************************************
 get.iid.dat.Abin <- function(ndata = 100000, rndseed = NULL, is.Y.bin = TRUE) {
   require(simcausal)
   D <- DAG.empty()
@@ -58,10 +61,10 @@ indSample.ind.bA.cY_list <- list(indSample.ind.bA.cY = indSample.ind.bA.cY, rnds
 save(indSample.ind.bA.cY_list, file="indSample.ind.bA.cY_list.Rda")
 
 
-################################################
-#### FUN 2. Rare outcome with binary Y (Case-control study)
-################################################
-get.iid.dat.Abin2 <- function(ndata = 100000, rndseed = NULL) {
+#**************************************************************
+# FUNCTION 2. Rare outcome with binary Y (Case-control study)
+#**************************************************************
+get.iid.dat.Abin.Rare <- function(ndata = 100000, rndseed = NULL) {
   require(simcausal)
   D <- DAG.empty()
   D <- D + 
@@ -107,71 +110,31 @@ drawSamples <- function(nCa, nCo, Cohort, rndseed = NULL, replace = FALSE) {
   return(list(samples = samples, obs.wt = obs.wt, J = J))
 }
 
-rndseed <- 12345
-cohort_iidBinABinY.rare <- get.iid.dat.Abin2(ndata = 1000000, rndseed = rndseed)$Odata
-q0 <- mean(cohort_iidBinABinY.rare$Y)  # 0.013579 Rare outcome!!
-psi0.Y <- mean(cohort_iidBinABinY.rare$Y1) - mean(cohort_iidBinABinY.rare$Y0) # 0.012662
+rndseed <- 12345 
+indPop.ind.bA.bY.rare <- get.iid.dat.Abin.Rare(ndata = 1000000, rndseed = rndseed)$Odata
+q0 <- mean(indPop.ind.bA.bY.rare$Y)  # 0.013579 Rare outcome!!
+psi0.Y <- mean(indPop.ind.bA.bY.rare$Y1) - mean(indPop.ind.bA.bY.rare$Y0) # 0.012662
 
 # ------------------------------
 # Sample set 1. J = 1
 # ------------------------------
 drawSamples.J1 <- drawSamples(nCa = 1000, nCo = 1000, rndseed = rndseed, replace = FALSE,
-                              Cohort = cohort_iidBinABinY.rare[, c("W1", "W2", "W3", "W4", "A", "Y")])
-dat_iidBinAY.rareJ1 <- drawSamples.J1$samples
+                              Cohort = indPop.ind.bA.bY.rare[, c("W1", "W2", "W3", "W4", "A", "Y")])
+indSample.ind.bA.bY.rareJ1 <- drawSamples.J1$samples
 obs.wt.J1 <- drawSamples.J1$obs.wt
-rareSamples_iidBinAY.J1 <- list(dat_iidBinAY.rareJ1 = dat_iidBinAY.rareJ1, obs.wt.J1 = obs.wt.J1,
-                                J = drawSamples.J1$J, q0 = q0, psi0.Y = psi0.Y, rndseed = rndseed, 
-                                call = list(Cohort = "get.iid.dat.Abin2(ndata = 1000000, rndseed = 12345)$Odata",
-                                            Sample = "drawSamples(nCa = 1000, nCo = 1000, rndseed = 12345, Cohort = Cohort)"))
-save(rareSamples_iidBinAY.J1, file = "rareSamples_iidBinAY.J1.Rda")
+indSample.ind.bA.bY.rareJ1_list <- 
+  list(indSample.ind.bA.bY.rareJ1 = indSample.ind.bA.bY.rareJ1, obs.wt.J1 = obs.wt.J1,
+       J = drawSamples.J1$J, q0 = q0, psi0.Y = psi0.Y, rndseed = rndseed)
+save(indSample.ind.bA.bY.rareJ1_list, file = "indSample.ind.bA.bY.rareJ1_list.rda")
 
 # ------------------------------
 # Sample set 1. J = 2
 # ------------------------------
 drawSamples.J2 <- drawSamples(nCa = 1000, nCo = 2000, rndseed = rndseed, replace = FALSE,
-                              Cohort = cohort_iidBinABinY.rare[, c("W1", "W2", "W3", "W4", "A", "Y")])
-dat_iidBinAY.rareJ2 <- drawSamples.J2$samples
+                              Cohort = indPop.ind.bA.bY.rare[, c("W1", "W2", "W3", "W4", "A", "Y")])
+indSample.ind.bA.bY.rareJ2 <- drawSamples.J2$samples
 obs.wt.J2 <- drawSamples.J2$obs.wt
-rareSamples_iidBinAY.J2 <- list(dat_iidBinAY.rareJ2 = dat_iidBinAY.rareJ2, obs.wt.J2 = obs.wt.J2,
-                                J = drawSamples.J2$J, q0 = q0, psi0.Y = psi0.Y, rndseed = rndseed, 
-                                call = list(Cohort = "get.iid.dat.Abin2(ndata = 1000000, rndseed = 12345)$Odata",
-                                            Sample = "drawSamples(nCa = 1000, nCo = 2000, rndseed = 12345, Cohort = Cohort)"))
-save(rareSamples_iidBinAY.J2, file = "rareSamples_iidBinAY.J2.Rda")
-
-
-################################################
-#### FUN 3. One Binary A with Continuous Y
-################################################
-get.iid.dat.Abin3 <- function(ndata = 100000, rndseed = NULL) {
-  require(simcausal)
-  D <- DAG.empty()
-    D <- D + 
-    node("W1", distr = "rbern", prob = 0.5) +
-    node("W2", distr = "rbern", prob = 0.3) +
-    node("W3", distr = "rnorm", mean = 0, sd = 0.3) +
-    node("W4", distr = "runif", min = 0, max = 1) +
-    node("A", distr = "rbern", prob = plogis(0.92 * W1 + W2 + 1.32 * W3 - W4  + 0.1)) +
-    node("Y", distr = "rnorm", mean = (2.8 * A + 2 * W1 + 1.5 * W2 + 0.5 * W3 - 3 * W4 - 3.5), sd = 1) + 
-    node("Y1", distr = "rnorm", mean = (2.8 * 1 + 2 * W1 + 1.5 * W2 + 0.5 * W3 - 3 * W4 - 3.5), sd = 1) +
-    node("Y0", distr = "rnorm", mean = (2.8 * 0 + 2 * W1 + 1.5 * W2 + 0.5 * W3 - 3 * W4 - 3.5), sd = 1)
-  
-  D <- set.DAG(D)
-  Odata <- sim(D, n = ndata, rndseed = rndseed)
-  psi0.Y <- mean(Odata$Y1) - mean(Odata$Y0)
-  print("psi0.Y: " %+% psi0.Y)  
-  
-  return(list(psi0.Y = psi0.Y, Odata = Odata))
-}
-
-ndata <- 10000
-rndseed <- 12345
-popDat <- get.iid.dat.Abin3(ndata = 1000000, rndseed = rndseed, is.Y.bin = TRUE)$Odata
-psi0.Y <- mean(popDat$Y1) - mean(popDat$Y0) # 0.348242
-
-dat_iidBinAContY <- get.iid.dat.Abin3(ndata = ndata, rndseed = rndseed)$Odata
-dat_iidBinAContY <- dat_iidBinAContY[, c("W1", "W2", "W3", "W4", "A", "Y")]
-sampleDat_iidBinAContY <- list(dat_iidBinAContY = dat_iidBinAContY, psi0.Y = psi0.Y, rndseed = rndseed,
-                               call = "get.iid.dat.Abin3(ndata = 10000, rndseed = 12345)$Odata")
-save(sampleDat_iidBinAContY, file="sampleDat_iidBinAContY.Rda")
-
-
+indSample.ind.bA.bY.rareJ2_list <- 
+  list(indSample.ind.bA.bY.rareJ2 = indSample.ind.bA.bY.rareJ2, obs.wt.J2 = obs.wt.J2,
+       J = drawSamples.J2$J, q0 = q0, psi0.Y = psi0.Y, rndseed = rndseed)
+save(indSample.ind.bA.bY.rareJ2_list, file = "indSample.ind.bA.bY.rareJ2_list.rda")
