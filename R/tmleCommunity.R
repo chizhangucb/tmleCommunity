7da188aba60bf61c15d1adbd38b6903a5323a4da
chizhangucb <chi.zhang@berkeley.edu>
2017-11-09 22:42:25 -0800

    Update tmleCommunity.R

--- R/tmleCommunity.R
+++ R/tmleCommunity.R
@@ -7,7 +7,7 @@
 #' @importFrom graphics axis barplot hist par text
 #' @importFrom methods is
 #' @importFrom stats approx quasibinomial binomial coef glm.control glm.fit plogis predict qlogis qnorm quantile rnorm runif 
-#' @importFrom stats terms var aggregate weighted.mean as.formula pnorm pt qt reformulate model.matrix family 
+#' @importFrom stats terms var aggregate weighted.mean as.formula pnorm pt qt reformulate model.matrix gaussian 
 #' @importFrom utils data head str
 #' @importFrom Hmisc wtd.var
 
