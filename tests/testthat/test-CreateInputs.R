0302261a7439f0120daf306864682a309f9fd480
chizhangucb <chi.zhang@berkeley.edu>
2017-11-09 20:53:19 -0800

    Update test-CreateInputs.R

--- tests/testthat/test-CreateInputs.R
+++ tests/testthat/test-CreateInputs.R
@@ -1,1 +1,10 @@
 context("Test CreateInputs")
+
+data("indSample.iid.cA.cY_list", package = "tmleCommunity")
+indSample.iid.cA.cY <- indSample.iid.cA.cY_list$indSample.iid.cA.cY
+
+test_that("Continuous outcome will always be bounded into [0, 1] when maptoYstar=TRUE", {
+  obsYvals <- indSample.iid.cA.cY$Y
+  boundedY.list <- CreateInputs(obsYvals, Qbounds = NULL, alpha = 0.995, maptoYstar = TRUE)
+  expect_equal(boundedY.list$Ystar * diff(boundedY.list$ab) + boundedY.list$ab[1], obsYvals)
+})
