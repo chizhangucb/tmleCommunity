2efc08cd70ba510733d7a0b938a2ab4e515c5d3f
chizhangucb <chi.zhang@berkeley.edu>
2017-11-13 09:58:50 -0800

    Create test-DatKeepClass-examples.R

---
+++ tests/testthat/test-DatKeepClass-examples.R
@@ -0,0 +1,14 @@
+context("Test DatKeepClass")
+
+`%+%` <- function(a, b) paste0(a, b)
+data("indSample.iid.cA.cY_list", package = "tmleCommunity")
+indSample.iid.cA.cY <- indSample.iid.cA.cY_list$indSample.iid.cA.cY
+tmleCom_Options(maxNperBin = nrow(indSample.iid.cA.cY))
+nodes <- list(Ynode = "Y", Anodes = "A", WEnodes = c("W1", "W2", "W3", "W4"))
+
+test_that("nodes' name can only be one or more of Ynode, Anodes, WEnodes, communityID and Crossnodes", {
+  nodes.bad <- list(Ynode = "Y", Anodes = "A", badnodes = c("W1", "W2", "W3", "W4"))
+  expect_error(
+    expect_message(DatKeepClass$new(Odata = indSample.iid.cA.cY, nodes = nodes.bad), "Don't recognize badnodes"), 
+    "It should be a list & its names can only be one or more of Ynode, Anodes, WEnodes, communityID and Crossnodes.")
+})
