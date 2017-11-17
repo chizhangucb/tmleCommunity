c4ab4790b925e65e349a636a91b55171a690c4f1
chizhangucb <chi.zhang@berkeley.edu>
2017-11-17 15:42:04 -0800

    Update test-tmleCommunity-clusterExmaples.R

--- tests/testthat/test-tmleCommunity-clusterExmaples.R
+++ tests/testthat/test-tmleCommunity-clusterExmaples.R
@@ -3,7 +3,7 @@ context("Test tmleCommunity on community-level exposure")
 # ---------------------------------------------------------------------------------
 # TEST SET 1. TESTS FOR FITTING BINARY EXPOSURE A IN HIERARCHICAL DATA
 # ---------------------------------------------------------------------------------
-get.cluster.dat.Abin <- function(id, n.ind = 10000, rndseed = NULL, is.Y.bin = TRUE) {
+get.cluster.dat.Abin <- function(id, n.ind = 10000, rndseed = NULL) {
   set.seed(rndseed)
   E1 <- runif(n = 1, min = 0, max = 1)
   E2 <- sample(x = c(0, 0.2, 0.4, 0.8, 1), size = 1)
@@ -24,65 +24,212 @@ get.cluster.dat.Abin <- function(id, n.ind = 10000, rndseed = NULL, is.Y.bin = T
   return(data.frame(cbind(id, E1, E2, W1, W2, W3, A, Y, Y1, Y0)))
 }
 
+get.fullDat.Abin <- function(J, n.ind, rndseed = NULL, n.ind.fix = FALSE, verbose = TRUE) {
+  set.seed(rndseed)
+  if (n.ind.fix) {
+    n.ind <- rep(n.ind, J)
+  } else {  # don't fix the number of individuals in each community
+    n.ind <- round(rnorm(J, n.ind, 10))  
+    n.ind[n.ind <= 0] <- n.ind  # set to n.ind if any generated number less than 1
+  }
+  full.data <- NULL
+
+  for(j in 1:J) {
+    if (verbose) message("#### generating " %+% j %+% "th cluster ####")
+    cluster.data.j <- get.cluster.dat.Abin(id = j, n.ind = n.ind[j], rndseed = eval(rndseed + j)) 
+    full.data <- rbind(full.data, cluster.data.j)
+  }  
+  full.data$id <- as.integer(full.data$id)
+  return(full.data)
+}
+
 `%+%` <- function(a, b) paste0(a, b)
 J <- 1000
 n.ind <- 50
 rndseed <- 12345
-comSample.wmF.bA.bY <- get.fullDat.Abin(J = J, n.ind = n.ind, rndseed = rndseed, is.Y.bin = TRUE, working.model = FALSE)
+comSample.wmF.bA.bY <- get.fullDat.Abin(J = J, n.ind = n.ind, rndseed = rndseed)
+mean(comSample.wmF.bA.bY$Y1) - mean(comSample.wmF.bA.bY$Y0) # 0.1753572
 comSample.wmF.bA.bY <- comSample.wmF.bA.bY[, c("id", "E1", "E2", "W1", "W2", "W3", "A", "Y")]
 N <- NROW(comSample.wmF.bA.bY)
 Qform.corr <- "Y ~ E1 + E2 + W1 + W2 + W3 + A" # correct Q form
 gform.corr <- "A ~ E1 + E2 + W1"  # correct g form
 
 #*************************************** 
-## Test 1.1 speed.glm & glm 
+## Test 1.1 Different analysis methods
 #*************************************** 
 tmleCom_Options(Qestimator = "speedglm__glm", gestimator = "speedglm__glm", maxNperBin = N)
 
 ## Test 1.1.1 Community-level analysis without a pooled individual-level regression on outcome
 test_that("fit TMLE for bin, community-level A, with community-level analysis without a pooled Q", {
-   tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
-                               WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
-                               f_gstar2 = 0L, community.step = "community_level", 
-                               communityID = "id", pooled.Q = FALSE, Qform = Qform.corr, 
-                               hform.g0 = gform.corr, hform.gstar = gform.corr)
-  estimates <- tmleCom_res$ATE$estimates  # psi0 = 
-  expect_equal(estimates["tmle", ],  , tolerance = 1e-6))
-  expect_equal(estimates["iptw", ],  , tolerance = 1e-6))
-  expect_equal(estimates["gcomp", ],  , tolerance = 1e-6))
-  expect_that()
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.step = "community_level", 
+                              communityID = "id", pooled.Q = FALSE)
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  expect_equal(estimates["tmle", ], 0.1773423, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1899487, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1813821, tolerance = 1e-6)
 })
 
 ## Test 1.1.2 Community-level analysis with a pooled individual-level regression on outcome
 test_that("fit TMLE for bin, community-level A, with community-level analysis with a pooled Q", {
-   tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
-                               WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
-                               f_gstar2 = 0L, community.step = "community_level", 
-                               communityID = "id", pooled.Q = TRUE, Qform = Qform.corr, 
-                               hform.g0 = gform.corr, hform.gstar = gform.corr)
-  estimates <- tmleCom_res$ATE$estimates  # psi0 = 
-  expect_equal(estimates["tmle", ],  , tolerance = 1e-6))
-  expect_equal(estimates["iptw", ],  , tolerance = 1e-6))
-  expect_equal(estimates["gcomp", ],  , tolerance = 1e-6))
-  expect_that()
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.step = "community_level", 
+                              communityID = "id", pooled.Q = TRUE)
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  expect_equal(estimates["tmle", ], 0.1784616, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1957239, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1838956, tolerance = 1e-6)
 })
 
-## Test 1.1.3 Individual-level analysis with both individual-level outcome and treatment mechanisms
+# Test 1.1.3 Individual-level analysis with both individual-level outcome and treatment mechanisms
+# the bias of estimation is larger than community-level analysis since the working model fails
 test_that("fit TMLE for bin, community-level A, with individual-level analysis", {
-   tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
-                               WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
-                               f_gstar2 = 0L, community.step = "individual_level", 
-                               communityID = "id", pooled.Q = TRUE, Qform = Qform.corr, 
-                               hform.g0 = gform.corr, hform.gstar = gform.corr)
-  estimates <- tmleCom_res$ATE$estimates  # psi0 = 
-  expect_equal(estimates["tmle", ],  , tolerance = 1e-6))
-  expect_equal(estimates["iptw", ],  , tolerance = 1e-6))
-  expect_equal(estimates["gcomp", ],  , tolerance = 1e-6))
-  expect_that()
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.step = "individual_level", communityID = "id")
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  variances <- tmleCom_res$ATE$vars
+  expect_equal(estimates["tmle", ], 0.1835877, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1854157, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1825362, tolerance = 1e-6)
+  expect_equal(as.vector(variances[, 1]), c(3.899418e-05, 2.864193e-03, 3.899418e-05))
 })
 
-#***************************************************************************************
+# Test 1.1.4 Individual-level analysis that assumes no hierarchical structure
+test_that("Treat hierarchical data as non-hierarchical and use usual TMLE", {
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.step = "NoCommunity")
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  variances <- tmleCom_res$ATE$vars
+  expect_equal(estimates["tmle", ], 0.1835877, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1854157, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1825362, tolerance = 1e-6)
+  expect_equal(as.vector(variances[, 1]), c(3.217030e-05, 8.865422e-05, 3.217030e-05))
+  
+  expect_message(
+    tmleCom_res2 <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                                 WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                                 f_gstar2 = 0L, community.step = "individual_level", 
+                                 communityID = NULL),
+    regexp = "Lack of 'communityID' forces the algorithm to automatically " %+%
+      "pool data over all communities and treat it as non-hierarchical dataset"
+  )
+  estimates2 <- tmleCom_res2$ATE$estimates  # psi0 = 0.1768159
+  expect_equal(estimates, estimates2)
+})
+
+## Test 1.1.5 Unweighted covariate-based TMLE, using community-level analysis without a pooled Q
+test_that("fit unweighted covariate-based TMLE for bin, community-level A", {
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.step = "community_level", 
+                              communityID = "id", TMLE.targetStep = "tmle.covariate")
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  expect_equal(estimates["tmle", ], 0.1764060, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1899487, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1813821, tolerance = 1e-6)
+})
+
+## Test 1.1.6 Weigh each community equally, using community-level analysis without a pooled Q
+test_that("fit unweighted covariate-based TMLE for bin, community-level A", {
+  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+                              f_gstar2 = 0L, community.wts = "equal.community",
+                              community.step = "community_level", communityID = "id")
+  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+  expect_equal(estimates["tmle", ], 0.1766701, tolerance = 1e-6)
+  expect_equal(estimates["iptw", ], 0.1907880, tolerance = 1e-6)
+  expect_equal(estimates["gcomp", ], 0.1809624, tolerance = 1e-6)
+})
+
+# Test 1.1.7 Stratified TMLE that fits a separate outcome (exposure) mechanism for each community
+#test_that("fit stratified TMLE for bin, community-level A", {
+#  tmleCom_Options(Qestimator = "glm__glm", gestimator = "glm__glm", maxNperBin = N)
+#  tmleCom_res <-tmleCommunity(data = comSample.wmF.bA.bY, Ynode = "Y", Anodes = "A",
+#                              WEnodes = c("E1", "E2", "W1", "W2", "W3"), f_gstar1 = 1L, 
+#                              f_gstar2 = 0L, community.step = "perCommunity", communityID = "id")
+#  estimates <- tmleCom_res$ATE$estimates  # psi0 = 0.1768159
+#  expect_equal(estimates["tmle", ], 0.03036185, tolerance = 1e-6)
+#  expect_equal(estimates["gcomp", ], 0.00000000, tolerance = 1e-6)
+#  expect_equal(as.vector(variances[, 1]), c(2.08973e-11, 3.41511e-04, 2.08973e-11))
+#})
 
 # ---------------------------------------------------------------------------------
 # TEST SET 2. TESTS FOR FITTING CONTINUOUS EXPOSURE A IN HIERARCHICAL DATA
 # ---------------------------------------------------------------------------------
+get.cluster.dat.Acont <- function(id, n.ind = 1000, truncBD = 5, shift.val = 1, rndseed = NULL) {
+  set.seed(rndseed)
+  # Construct community-level & individual-level baseline covariates E, W 
+  E1 <- runif(n = 1, min = 0, max = 1)
+  E2 <- sample(x = c(0.2, 0.4, 0.6, 0.8), size = 1)
+  W1 <- rbinom(n = n.ind, size = 1, prob = plogis(- 0.4 + 1.2 * E1 - 1.3 * E2))
+  W2_mean <- 1 - 0.8 * E1 - 0.4 * E2 
+  W3_mean <- 0.5 + 0.2 * E1
+  W2W3 <- MASS::mvrnorm(n = n.ind, mu = c(W2_mean, W3_mean), Sigma = matrix(c(1, 0.6, 0.6, 1), ncol = 2))
+  W2 <- W2W3[, 1]
+  W3 <- W2W3[, 2] 
+  A.mu <- - 1.2 + 0.8 * E1 + 0.21 * E2 + 3 * mean(W1) - 0.7 * mean(W2) + 0.3 * mean(W3)
+  A <- rnorm(n = 1, mean = A.mu, sd = 1)
+  untrunc.A.gstar <- A + shift.val
+  r.new.A <- exp(1.5 * shift.val * (untrunc.A.gstar - A.mu - shift.val / 4))
+  trunc.A.gstar <- ifelse(r.new.A > truncBD, A, untrunc.A.gstar)
+  Y <- rbinom(n = n.ind, size = 1, 
+              prob = plogis(- 1.7 + 1.2 * A + 0.5 * E1 - 1.2 * E2 + 0.7 * W1 + 1.3 * W2 - 0.4 * W3))
+  Y.gstar <- rbinom(n = n.ind, size = 1,
+                    prob = plogis(- 1.7 + 1.2 * trunc.A.gstar + 0.5 * E1 - 1.2 * E2 + 0.7 * W1 + 1.3 * W2 - 0.4 * W3))
+  return(data.frame(cbind(id, E1, E2, W1, W2, W3, A, trunc.A.gstar, Y, Y.gstar)))
+}
+
+get.fullDat.Acont <- function(J, n.ind, rndseed = NULL, truncBD = 5, shift.val = 1, 
+                              n.ind.fix = FALSE, verbose = TRUE) {
+  set.seed(rndseed)
+  if (n.ind.fix) {
+    n.ind <- rep(n.ind, J)
+  } else {  # don't fix the number of individuals in each community
+    n.ind <- round(rnorm(J, n.ind, 10))  
+    n.ind[n.ind <= 0] <- n.ind  # set to n.ind if any generated number less than 1
+  }
+  full.data <- NULL
+  
+  for(j in 1:J) {
+    if (verbose) message("#### generating " %+% j %+% "th cluster ####")
+    cluster.data.j <- get.cluster.dat.Acont(id = j, n.ind = n.ind[j], truncBD = truncBD, 
+                                            shift.val = shift.val, rndseed = eval(rndseed + j)) 
+    full.data <- rbind(full.data, cluster.data.j)
+  }  
+  full.data$id <- as.integer(full.data$id)
+  return(full.data)
+}
+
+J <- 1000
+n.ind <- 100
+rndseed <- 12345
+truncBD <- 5
+shift.val <- 1
+comSample.wmT.cA.bY <- get.fullDat.Acont(J = J, n.ind = n.ind, rndseed = rndseed, 
+                                         truncBD = truncBD, shift.val = shift.val)
+mean(comSample.wmT.cA.bY$Y)  # 0.3478304
+mean(comSample.wmT.cA.bY$Y.gstar)  # 0.4642384
+
+define_f.gstar <- function(shift.val, truncBD, rndseed = NULL) {
+  shift.const <- shift.val
+  trunc.const <- truncBD
+  f.gstar <- function(data, ...) {
+    print("shift.const: " %+% shift.const)
+    set.seed(rndseed)
+    A.mu <- - 1.2 + 0.8 * data[, "E1"] + 0.21 * data[,"E2"] + 3 * mean(data[,"W2"]) - 
+      0.7 * mean(data[,"W2"]) + 0.3 * mean(data[,"W3"])
+    untrunc.A <- rnorm(n = nrow(data), mean = A.mu + shift.const, sd = 1)
+    r.new.A <- exp(1.5 * shift.const * (untrunc.A - A.mu - shift.const / 4))
+    trunc.A <- ifelse(r.new.A > trunc.const, untrunc.A - shift.const, untrunc.A)
+    return(trunc.A)
+  }
+  return(f.gstar)
+}
+f.gstar <- define_f.gstar(shift.val = shift.val, truncBD = truncBD)
+
+
+#***************************************************************************************
