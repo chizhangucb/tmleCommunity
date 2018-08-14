0e22c6998825973d92e9aeb99b99bc9d86613db6
chizhangucb <chi.zhang@berkeley.edu>
2018-08-14 14:53:20 -0700

    Update test-all.R

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,5 +1,6 @@
 library(testthat)
 library(tmleCommunity)
-install_github("h2oai/h2o-3/h2o-r/ensemble/h2oEnsemble-package")
+library(devtools)
+devtools::install_github("h2oai/h2o-3/h2o-r/ensemble/h2oEnsemble-package")
 
 test_check("tmleCommunity")
